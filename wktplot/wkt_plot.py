8f64e1256a3580fa15b2cbdff5d8f56b6c655c90
Michael Simpson <mikeysimpson4@gmail.com>
2021-04-11 18:05:15 -0700

    Disable all methods except for `plot_line()` and `save()`

--- wktplot/wkt_plot.py
+++ wktplot/wkt_plot.py
@@ -12,6 +12,8 @@ from shapely.geometry import GeometryCollection, LineString, MultiLineString, Mu
 
 class WKTPlot:
     def __init__(self, title: ty.Optional[str]):
+        """ TODO: docstring
+        """
         if title is None:
             title = f"{int(time())}"
         self.figure = figure(title=title, x_axis_label="Longitude", y_axis_label="Latitude")
@@ -31,77 +33,68 @@ class WKTPlot:
             self.__zorder += 1
 
     def plot_poly(self, obj, fill_color, stroke_color):
+        """ TODO: docstring
         """
-        Plot given Polygon `obj`, filled with given `color`, and a white edge color.
-
-        Args:
-            obj (shapely.Polygon): Polygon to plot.
-            color (str): Color hex-string from `matplotlib.colors.CSS4_COLORS` dictionary.
-        """
-        patch = PolygonPatch(obj, fc=fill_color, ec=stroke_color, zorder=self.__zorder)
-        self.ax.add_patch(patch)
-        self.ax.plot()
-        self.__zorder += 1
+        pass
+        # patch = PolygonPatch(obj, fc=fill_color, ec=stroke_color, zorder=self.__zorder)
+        # self.ax.add_patch(patch)
+        # self.ax.plot()
+        # self.__zorder += 1
 
     def add_geodataframe(self, gdf, fill_color=None, stroke_color=None, name=None):
-        fill_color = colors_dict.get(fill_color, "#FFFFFFFF")
-        stroke_color = colors_dict.get(stroke_color, "#FFFFFFFF")
-        gdf.plot(ax=self.ax, fc=fill_color, ec=stroke_color, zorder=self.__zorder)
-        self.__zorder += 1
+        """ TODO: docstring
+        """
+        pass
+        # fill_color = colors_dict.get(fill_color, "#FFFFFFFF")
+        # stroke_color = colors_dict.get(stroke_color, "#FFFFFFFF")
+        # gdf.plot(ax=self.ax, fc=fill_color, ec=stroke_color, zorder=self.__zorder)
+        # self.__zorder += 1
 
     def add_shape(self, shape, fill_color=None, stroke_color=None, name=None):
+        """ TODO: docstring
         """
-        Add given `shape` to created figure, filled with the given `color`. Save `shape`'s WKT to
-        txt file with given `name`, if `name` is not None.
+        pass
+        # if isinstance(shape, str):
+        #     shape = wkt.loads(shape)
 
-        Args:
-            shape (str, obj: shapely.geometry): Shape to plot.
-            color (str): Color name for shape. Looked up in 'matplotlib.colors.CSS4_COLORS' dict.
-                e.g. 'gold' --> '#FFD700', 'aquamarine' --> '#7FFFD4'
-            name (optional, default=None): Name of WKT text file to save shape to. Saves to class
-            variable `save_dir` in 'WKT' subfolder.
-        """
-        if isinstance(shape, str):
-            shape = wkt.loads(shape)
+        # fill_color = colors_dict.get(fill_color, "#00000000")
+        # stroke_color = colors_dict.get(stroke_color, "#00000000")
+        # if not fill_color and not stroke_color:
+        #     print("No fill or stroke color.")
+        #     return
 
-        fill_color = colors_dict.get(fill_color, "#00000000")
-        stroke_color = colors_dict.get(stroke_color, "#00000000")
-        if not fill_color and not stroke_color:
-            print("No fill or stroke color.")
-            return
+        # if isinstance(shape, MultiLineString):
+        #     for line_string in shape:
+        #         self.plot_line(line_string, fill_color)
+        # elif isinstance(shape, (LineString, Point)):
+        #     self.plot_line(shape, fill_color)
+        # elif isinstance(shape, MultiPolygon):
+        #     for poly in shape:
+        #         self.plot_poly(poly, fill_color, stroke_color)
+        # elif isinstance(shape, Polygon):
+        #     self.plot_poly(shape, fill_color, stroke_color)
+        # elif isinstance(shape, GeometryCollection):
+        #     for poly in shape:
+        #         self.add_shape(poly, fill_color=fill_color, stroke_color=stroke_color)
+        # else:
+        #     print(type(shape))
 
-        if isinstance(shape, MultiLineString):
-            for line_string in shape:
-                self.plot_line(line_string, fill_color)
-        elif isinstance(shape, (LineString, Point)):
-            self.plot_line(shape, fill_color)
-        elif isinstance(shape, MultiPolygon):
-            for poly in shape:
-                self.plot_poly(poly, fill_color, stroke_color)
-        elif isinstance(shape, Polygon):
-            self.plot_poly(shape, fill_color, stroke_color)
-        elif isinstance(shape, GeometryCollection):
-            for poly in shape:
-                self.add_shape(poly, fill_color=fill_color, stroke_color=stroke_color)
-        else:
-            print(type(shape))
-
-        self.save_wkt(shape.wkt, name)
+        # self.save_wkt(shape.wkt, name)
 
     def add_shapes(self, shapes_list):
+        """ TODO: docstring
         """
-        Bulk plot multiple shapes.
-        """
-        for item in shapes_list:
-            self.add_shape(*item)
-        return self
+        pass
+        # for item in shapes_list:
+        #     self.add_shape(*item)
+        # return self
 
     def clear(self):
+        """ TODO: docstring
         """
-        Clear current figure and initialize new axis.
-        """
-        self.fig.clf()
-        self.setup_axis()
+        pass
+        # self.fig.clf()
+        # self.setup_axis()
 
         """
 
@@ -120,12 +113,15 @@ class WKTPlot:
         return self
 
     def save_wkt(self, wkt, name):
-        if name is not None:
-            if not os.path.isdir(self.wkt_dir):
-                os.mkdir(self.wkt_dir)
-            name = name.lower().replace(" ", "_")
-            wkt_f = os.path.join(self.wkt_dir, f"{name}.txt")
-            if os.path.isfile(wkt_f):
-                os.remove(wkt_f)
-            with open(wkt_f, "w+") as wf:
-                wf.write(wkt)
+        """ TODO: docstring
+        """
+        pass
+        # if name is not None:
+        #     if not os.path.isdir(self.wkt_dir):
+        #         os.mkdir(self.wkt_dir)
+        #     name = name.lower().replace(" ", "_")
+        #     wkt_f = os.path.join(self.wkt_dir, f"{name}.txt")
+        #     if os.path.isfile(wkt_f):
+        #         os.remove(wkt_f)
+        #     with open(wkt_f, "w+") as wf:
+        #         wf.write(wkt)
