8f6ae7bfd62413fef6cbad955dccc3e33e51bda0
Michael Simpson <mikeysimpson4@gmail.com>
2021-04-18 15:31:47 -0700

    Rename `internal_functions_tests` to `test_internal_functions`. Add style_kwargs to internal method calls where applicable

--- test/test_internal_functions.py
+++ test/test_internal_functions.py
@@ -7,6 +7,7 @@ import unittest
 
 PLOT_TITLE = "test_1234"
 PLOT_SAVE_DIR = "/tmp/mock_me_up_scotty"
+STYLE_KWARGS = {"color": "MidnightBlue", "line_width": 3.0}
 
 class InternalFunctionTests(unittest.TestCase):
 
@@ -29,26 +30,26 @@ class InternalFunctionTests(unittest.TestCase):
         """
         test_plot = self.setup_plot()
 
-        test_plot._plot_points(wkt.loads("POINT (30 10)"))
+        test_plot._plot_points(wkt.loads("POINT (30 10)"), **STYLE_KWARGS)
         expected_x = [30]
         expected_y = [10]
-        test_plot.figure.circle.assert_called_once_with(expected_x, expected_y, line_width=3)
+        test_plot.figure.circle.assert_called_once_with(expected_x, expected_y, **STYLE_KWARGS)
 
     def test__plot_points__verify_multipoint(self):
         """ Verify `_plot_points` calls methods with expected arguments when given `MultiPoint` shape.
         """
         test_plot = self.setup_plot()
-        test_plot._plot_points(wkt.loads("MULTIPOINT ((10 40), (40 30), (20 20), (30 10))"))
+        test_plot._plot_points(wkt.loads("MULTIPOINT ((10 40), (40 30), (20 20), (30 10))"), **STYLE_KWARGS)
         expected_x = [10, 40, 20, 30]
         expected_y = [40, 30, 20, 10]
-        test_plot.figure.circle.assert_called_once_with(expected_x, expected_y, line_width=3)
+        test_plot.figure.circle.assert_called_once_with(expected_x, expected_y, **STYLE_KWARGS)
     
     def test__plot_points__verify_empty(self):
         """ Verify `_plot_points` calls methods with expected arguments when given empty shape.
         """
         test_plot = self.setup_plot()
         with patch.object(test_plot, "logger", Mock(spec=logging)) as mock_logger:
-            test_plot._plot_points(wkt.loads("POINT EMPTY"))
+            test_plot._plot_points(wkt.loads("POINT EMPTY"), **STYLE_KWARGS)
             mock_logger.info.assert_called_once_with("Given shape is empty, returning.")
             test_plot.figure.circle.assert_not_called()
     
@@ -57,7 +58,7 @@ class InternalFunctionTests(unittest.TestCase):
         """
         test_plot = self.setup_plot()
         with self.assertRaises(TypeError) as e:
-            test_plot._plot_points(wkt.loads("POLYGON ((1 0, 1 1, 0 1, 0 0, 1 0))"))
+            test_plot._plot_points(wkt.loads("POLYGON ((1 0, 1 1, 0 1, 0 0, 1 0))"), **STYLE_KWARGS)
         self.assertIn("Given `shape` argument is of an unexpected type [Polygon]", e.exception.args)
     
     # --- _plot_lines tests ---
@@ -65,35 +66,35 @@ class InternalFunctionTests(unittest.TestCase):
         """ Verify `_plot_lines` calls methods with expected arguments when given `LineString` shape.
         """
         test_plot = self.setup_plot()
-        test_plot._plot_lines(wkt.loads("LINESTRING (30 10, 10 30, 40 40)"))
+        test_plot._plot_lines(wkt.loads("LINESTRING (30 10, 10 30, 40 40)"), **STYLE_KWARGS)
         expected_x = [30, 10, 40]
         expected_y = [10, 30, 40]
-        test_plot.figure.line.assert_called_once_with(expected_x, expected_y, line_width=3)
+        test_plot.figure.line.assert_called_once_with(expected_x, expected_y, **STYLE_KWARGS)
     
     def test__plot_lines__verify_linear_ring(self):
         """ Verify `_plot_lines` calls methods with expected arguments when given `LinearRing` shape.
         """
         test_plot = self.setup_plot()
-        test_plot._plot_lines(wkt.loads("LINEARRING (10 20, 20 25, 35 50, 10 20)"))
+        test_plot._plot_lines(wkt.loads("LINEARRING (10 20, 20 25, 35 50, 10 20)"), **STYLE_KWARGS)
         expected_x = [10, 20, 35, 10]
         expected_y = [20, 25, 50, 20]
-        test_plot.figure.line.assert_called_once_with(expected_x, expected_y, line_width=3)
+        test_plot.figure.line.assert_called_once_with(expected_x, expected_y, **STYLE_KWARGS)
 
     def test__plot_lines__verify_multilinestring(self):
         """ Verify `_plot_lines` calls methods with expected arguments when given `MultiLineString` shape.
         """
         test_plot = self.setup_plot()
-        test_plot._plot_lines(wkt.loads("MULTILINESTRING ((10 10, 20 20, 10 40), (40 40, 30 30, 40 20, 30 10))"))
+        test_plot._plot_lines(wkt.loads("MULTILINESTRING ((10 10, 20 20, 10 40), (40 40, 30 30, 40 20, 30 10))"), **STYLE_KWARGS)
         expected_x = [[10, 20, 10], [40, 30, 40, 30]]
         expected_y = [[10, 20, 40], [40, 30, 20, 10]]
-        test_plot.figure.multi_line.assert_called_once_with(expected_x, expected_y, line_width=3)
+        test_plot.figure.multi_line.assert_called_once_with(expected_x, expected_y, **STYLE_KWARGS)
 
     def test__plot_lines__verify_empty(self):
         """ Verify `_plot_lines` calls methods with expected arguments when given empty shape.
         """
         test_plot = self.setup_plot()
         with patch.object(test_plot, "logger", Mock(spec=logging)) as mock_logger:
-            test_plot._plot_lines(wkt.loads("LINESTRING EMPTY"))
+            test_plot._plot_lines(wkt.loads("LINESTRING EMPTY"), **STYLE_KWARGS)
             mock_logger.info.assert_called_once_with("Given shape is empty, returning.")
             test_plot.figure.line.assert_not_called()
             test_plot.figure.multi_line.assert_not_called()
@@ -103,7 +104,7 @@ class InternalFunctionTests(unittest.TestCase):
         """
         test_plot = self.setup_plot()
         with self.assertRaises(TypeError) as e:
-            test_plot._plot_lines(wkt.loads("POLYGON ((1 0, 1 1, 0 1, 0 0, 1 0))"))
+            test_plot._plot_lines(wkt.loads("POLYGON ((1 0, 1 1, 0 1, 0 0, 1 0))"), **STYLE_KWARGS)
         self.assertIn("Given `shape` argument is of an unexpected type [Polygon]", e.exception.args)
 
     # --- _plot_polys tests ---
@@ -114,8 +115,8 @@ class InternalFunctionTests(unittest.TestCase):
         coords = ([[30.0, 40.0, 20.0, 10.0]], [[10.0, 40.0, 40.0, 20.0]])
         with patch.object(test_plot, "_get_poly_coordinates") as mock_coords:
             mock_coords.return_value = coords
-            test_plot._plot_polys(wkt.loads("POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))"))
-            test_plot.figure.multi_polygons.assert_called_once_with([[coords[0]]], [[coords[1]]], line_width=3)
+            test_plot._plot_polys(wkt.loads("POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))"), **STYLE_KWARGS)
+            test_plot.figure.multi_polygons.assert_called_once_with([[coords[0]]], [[coords[1]]], **STYLE_KWARGS)
     
     def test__poly_polys__verify_multipolygon(self):
         """ Verify `_plot_polys` calls methods with expected arguments when given `MultiPolygon` shape.
@@ -124,16 +125,15 @@ class InternalFunctionTests(unittest.TestCase):
         coords = ([[30.0, 45.0, 10.0], [15.0, 40.0, 10.0, 5.0]], [[20.0, 40.0, 40.0], [5.0, 10.0, 20.0, 10.0]])
         with patch.object(test_plot, "_get_poly_coordinates") as mock_coords:
             mock_coords.return_value = coords
-            test_plot._plot_polys(wkt.loads("MULTIPOLYGON (((30 20, 45 40, 10 40, 30 20)), ((15 5, 40 10, 10 20, 5 10, 15 5)))"))
-            
-            test_plot.figure.multi_polygons.assert_called_once_with([[coords[0]]], [[coords[1]]], line_width=3)
+            test_plot._plot_polys(wkt.loads("MULTIPOLYGON (((30 20, 45 40, 10 40, 30 20)), ((15 5, 40 10, 10 20, 5 10, 15 5)))"), **STYLE_KWARGS)
+            test_plot.figure.multi_polygons.assert_called_once_with([[coords[0]]], [[coords[1]]], **STYLE_KWARGS)
     
     def test__plot_polys__verify_empty(self):
         """ Verify `_plot_polys` calls methods with expected arguments when given empty shape.
         """
         test_plot = self.setup_plot()
         with patch.object(test_plot, "logger", Mock(spec=logging)) as mock_logger:
-            test_plot._plot_polys(wkt.loads("POLYGON EMPTY"))
+            test_plot._plot_polys(wkt.loads("POLYGON EMPTY"), **STYLE_KWARGS)
             mock_logger.info.assert_called_once_with("Given shape is empty, returning.")
             test_plot.figure.multi_polygons.assert_not_called()
     
@@ -142,7 +142,7 @@ class InternalFunctionTests(unittest.TestCase):
         """
         test_plot = self.setup_plot()
         with self.assertRaises(TypeError) as e:
-            test_plot._plot_polys(wkt.loads("LINEARRING (10 20, 20 25, 35 50, 10 20)"))
+            test_plot._plot_polys(wkt.loads("LINEARRING (10 20, 20 25, 35 50, 10 20)"), **STYLE_KWARGS)
         self.assertIn("Given `shape` argument is of an unexpected type [LinearRing]", e.exception.args)
 
     # --- _get_poly_coordinates tests ---
@@ -181,4 +181,3 @@ class InternalFunctionTests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-        
