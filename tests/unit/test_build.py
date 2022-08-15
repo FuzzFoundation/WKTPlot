bdd56b24564ddc251b1c44cf0a3b5eb94eed4765
misimpso <mikeysimpson4@gmail.com>
2022-08-15 12:41:35 -0700

    Add unittest for folder structure

---
+++ tests/unit/test_build.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+
+
+SOURCE_PATH = Path(__file__).parent / ".." / ".." / "src" / "wktplot"
+
+
+class TestFolderStructure:
+    def test_source_structure_has_init_files(self):
+        assert SOURCE_PATH.is_dir()
+        for directory, _, files in os.walk(SOURCE_PATH):
+            directory = Path(directory)
+            if directory.name == "__pycache__":
+                continue
+            assert "__init__.py" in files, f"Folder missing __init__.py file, {directory.resolve()}"
